866b7fa2c25e8bbb559428fb348ccf2a35d32e9d
Kevin Musselman <kmussel@gmail.com>
2020-01-07 13:22:49 -0500

    move migrations dir

--- ancilla/foundation/data/db.py
+++ ancilla/foundation/data/db.py
@@ -10,11 +10,15 @@ from peewee import SqliteDatabase
 from playhouse.sqlite_ext import SqliteExtDatabase
 from peewee_migrate import Router
 from ..env  import Env
+import os
+
+DEFAULT_MIGRATE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../migrations')
+# DEFAULT_MIGRATE_DIR = os.path.join(os, 'migrations')
 
 class Database(object):
   path = "/".join([Env.ancilla, ".a_store"])
   conn = SqliteExtDatabase(path, {'foreign_keys' : 1})
-  router = Router(conn)
+  router = Router(conn, migrate_dir=DEFAULT_MIGRATE_DIR)
 
   @staticmethod
   def connect():
